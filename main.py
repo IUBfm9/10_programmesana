a066688af8e8903726ec77a01cc5413380de605a
dainireR <101051579+dainireR@users.noreply.github.com>
2026-09-22 13:17:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+a=10
+x=7
+b=6
+y=a*x+b
+print(y)
+'''
+'''
+x=float(input("ātrums: "))#ātrums
+n=5 #stundas
+att=x*n#attālums
+print(att)
+'''
+'''
+a,b,c=2,5,9
+x=-b+a*a-4*c
+print(x)
+'''
+a,b,c=input("a,b,c: ").split()#datu tips virknes
+#atdalīs mainīgos
+#a=float(a)
+cena_PVN=5*float(a)*1.21+7*float(b)*1.21+4*float(c)*1.21
+print(cena_PVN)
